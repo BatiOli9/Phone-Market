2bae426cae0133ff274c679f9d164d64d00a6fb3
BatiOli9 <oliversafdieh@gmail.com>
2023-04-20 09:05:43 -0300

    Mejoras en el codigo

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -4,7 +4,12 @@ const router = express.Router();
 
 const productsController = require("../controllers/productsController");
 
+/* Todos los productos */
 router.get('/products', productsController.products);
+
+/* Detalle de los productos */
 router.get("/detalleProducto/:id/", productsController.productoDetalle);
+
+/* Formulario de creacion de productos */
 
 module.exports = router;
